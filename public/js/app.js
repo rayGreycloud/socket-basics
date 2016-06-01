c440845989646709d84c07fd75cde6492a9fe612
rayGreycloud <rayGreycloud@gmail.com>
2016-06-01 13:25:28 -0700

    add timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,13 @@ socket.on('connect', function() {
 });
 
 socket.on('message', function(message) {
+  var momentTimestamp = moment.utc(message.timestamp);
+
   console.log('New Message:');
   console.log(message.text);
 
-  jQuery('.messages').append('<p>' + message.text + '</p>');
+  jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format(
+    'h:mm a') + ': </strong>' + message.text + '</p>');
 });
 
 // new message submission handler
